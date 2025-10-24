70864fd4c421399e970f4c86d1e0b5078300e24e
varraschin <marcelojuniber@gmail.com>
2025-10-24 12:20:10 -0300

    Criado Variáveis de estado

--- GMovies/screens/HomeScreen.js
+++ GMovies/screens/HomeScreen.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Platform, TouchableOpacity, ScrollView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { Bars3BottomLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 import { styles } from '../theme';
+import TrendingMovies from '../components/trendingMovies';
 
 const ios = Platform.OS == "ios";
 
 export default function HomeScreen() {
+  const [trending, setTrending] = useState([1, 2, 3])
   return (
     <View className="flex-1 bg-neutral-800">
-      <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+      <View className={ios ? "-mb-2" : "mb-3"}>
         <StatusBar style='light' />
-        <View className="flex-row justify-between items-center mx-4 mt-2">
+        <View className="flex-row justify-between items-center mx-4 mt-8">
           <TouchableOpacity>
             <Bars3BottomLeftIcon size={30} strokeWidth={2} color="white" />
           </TouchableOpacity>
@@ -23,12 +24,12 @@ export default function HomeScreen() {
             <MagnifyingGlassIcon size={30} strokeWidth={2} color="white"/>
           </TouchableOpacity>
         </View>
-      </SafeAreaView>
+      </View>
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{paddingBottom: 10}}
       >
-
+        <TrendingMovies data={trending} />
       </ScrollView>
     </View>
   )
